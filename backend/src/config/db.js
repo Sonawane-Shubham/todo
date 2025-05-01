4799af0ded3150a5758be98d8a3579d119f00fc7
Sonawane-Shubham <sssonawane047@gmail.com>
2025-05-01 09:50:15 +0530

    change database url

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI_LOCAL);
+    await mongoose.connect(process.env.MONGO_URI_REMOTE);
     console.log("MongoDB database connected");
   } catch (err) {
     console.error(err.message);
